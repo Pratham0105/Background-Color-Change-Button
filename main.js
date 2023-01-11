c394ba510b1920d4dff12d02cd33c09b8c1a434e
Pratham Parashar <pratham.parashar143858@gmail.com>
2023-01-11 22:30:13 +0530

    Background Color

---
+++ main.js
@@ -0,0 +1,12 @@
+const button=document.querySelector("button")
+const body=document.querySelector("body")
+const color=['red','yellow','pink','yellow','purple']
+
+body.style.backgroundColor='violet'
+
+button.addEventListener('click', changeB)
+
+function changeB(){
+    const colorIndex=parseInt(Math.random()*color.length)
+    body.style.backgroundColor=color[colorIndex]
+}
